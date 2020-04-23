a9bfc7145a4077ca70c055c8d171f04038c75078
AdelchiPelizzo <streamsalesforce@gmail.com>
2020-04-23 11:23:13 +0200

    original stage

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const bodyParser= require('body-parser');
 const app = express();
-// const https = require('https');
+
 // Make sure you place body-parser before your CRUD handlers!
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.set('port', process.env.PORT || 3008);
-// app.listen(port, function() {
-//     console.log('listening on 3008')
-// });
+app.listen(3008, function() {
+    console.log('listening on 3008')
+});
 app.get('/:page/:label', (req, res) => {
     console.log(req.params.page);
     console.log(req.params.label);
@@ -22,7 +21,4 @@ app.get('/labels', (req, res) => {
 });
 app.post('/quotes', (req, res) => {
     console.log(req.body)
-});
-// https.createServer(app).listen(process.env.port || 3008, function() {
-//     console.log('Express app started');
-// });
+}); 
